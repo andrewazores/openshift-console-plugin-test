14e7c5a066812a1a5568aa32b195a148afbfd117
Andrew Azores <aazores@redhat.com>
2024-07-11 15:18:57 -0400

    get text from backend service

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,7 +1,11 @@
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { concatMap } from 'rxjs/operators';
 
 export class ApiService {
     getTest(): Observable<string> {
-        return of('Hello from the ApiService');
+        return fromFetch('http://localhost:9898/test', {
+            method: 'GET'
+        }).pipe(concatMap((resp: Response) => resp.text()));
     }
 }
