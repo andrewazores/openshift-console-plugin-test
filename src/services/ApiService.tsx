caaefbdf5aa93803a0b17f7ed2ff612cad641042
Andrew Azores <aazores@redhat.com>
2024-07-11 15:20:32 -0400

    add todo

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -4,6 +4,7 @@ import { concatMap } from 'rxjs/operators';
 
 export class ApiService {
     getTest(): Observable<string> {
+        // TODO the backend URL must be configurable
         return fromFetch('http://localhost:9898/test', {
             method: 'GET'
         }).pipe(concatMap((resp: Response) => resp.text()));
