7e636b1b1490a4dbf5b415d26cd8f40406adf292
Andrew Azores <aazores@redhat.com>
2024-07-12 10:06:20 -0400

    error handling

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,12 +1,18 @@
-import { Observable } from 'rxjs';
+import { of, Observable } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
-import { concatMap } from 'rxjs/operators';
+import { catchError, concatMap } from 'rxjs/operators';
 
 export class ApiService {
-    getTest(): Observable<string> {
-        // TODO the backend URL must be configurable
-        return fromFetch('http://localhost:9898/test', {
-            method: 'GET'
-        }).pipe(concatMap((resp: Response) => resp.text()));
-    }
+  getTest(): Observable<string> {
+    // TODO the backend URL must be configurable
+    return fromFetch('http://localhost:9898/test', {
+      method: 'GET'
+    }).pipe(
+      concatMap((resp: Response) => resp.ok ? resp.text() : of(resp.statusText)),
+      catchError(err => {
+        console.error(err);
+        return JSON.stringify(err);
+      }),
+    );
+  }
 }
