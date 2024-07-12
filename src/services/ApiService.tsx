957c9e98f72d1a622d0b079a7c83d18fff44f4b6
Andrew Azores <aazores@redhat.com>
2024-07-12 12:23:55 -0400

    use sdk consoleFetch

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,19 +1,20 @@
-import { of, Observable } from 'rxjs';
-import { fromFetch } from 'rxjs/fetch';
+import { from, of, Observable } from 'rxjs';
 import { catchError, concatMap } from 'rxjs/operators';
+import { consoleFetch } from '@openshift-console/dynamic-plugin-sdk';
 
 export class ApiService {
   getTest(): Observable<string> {
     // TODO the backend URL must be configurable somehow, or we can reconstruct it using
     // an assumed <svc-name>-<plugin-namespace> and the current document location (ie console URL)?
-    return fromFetch('http://cryostat-svc-plugin--cryostat.apps-crc.testing/test', {
-      method: 'GET'
-    }).pipe(
-      concatMap((resp: Response) => resp.ok ? resp.text() : of(resp.statusText)),
-      catchError(err => {
-        console.error(err);
-        return JSON.stringify(err);
-      }),
-    );
+    return from(
+      consoleFetch('http://cryostat-svc-plugin--cryostat.apps-crc.testing/test', {
+        method: 'GET'
+      })).pipe(
+        concatMap((resp: Response) => resp.ok ? resp.text() : of(resp.statusText)),
+        catchError(err => {
+          console.error(err);
+          return JSON.stringify(err);
+        }),
+      );
   }
 }
