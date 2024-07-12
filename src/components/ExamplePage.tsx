4150b933893e36bd1ffd729a4e4e952e31ef07a7
Andrew Azores <aazores@redhat.com>
2024-07-12 09:50:57 -0400

    remove circlecheckicon which somehow won't render

--- src/components/ExamplePage.tsx
+++ src/components/ExamplePage.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Helmet from 'react-helmet';
 import { useTranslation } from 'react-i18next';
 import { Page, PageSection, Text, TextContent, Title } from '@patternfly/react-core';
-import { CheckCircleIcon } from '@patternfly/react-icons';
 import './example.css';
 import {ServiceContext} from '../services/Services';
 
@@ -27,12 +26,6 @@ export default function ExamplePage() {
         </PageSection>
         <PageSection variant="light">
           <TextContent>
-            <Text component="p">
-              <span className="console-plugin-template__nice">
-                <CheckCircleIcon /> {t('Success!')}
-              </span>{' '}
-              {t('Your plugin is working.')}
-            </Text>
             <Text component="p">
               {t(
                 'This is a custom page contributed by the console plugin template. The extension that adds the page is declared in console-extensions.json in the project root along with the corresponding nav item. Update console-extensions.json to change or add extensions. Code references in console-extensions.json must have a corresponding property',
