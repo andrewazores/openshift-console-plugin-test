3d6d43ac209c97f8e0ef60b6e6d6340678ddfcf5
Andrew Azores <aazores@redhat.com>
2024-07-12 11:24:40 -0400

    hack to hardcode plugin backend svc url

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -4,8 +4,9 @@ import { catchError, concatMap } from 'rxjs/operators';
 
 export class ApiService {
   getTest(): Observable<string> {
-    // TODO the backend URL must be configurable
-    return fromFetch('http://localhost:9898/test', {
+    // TODO the backend URL must be configurable somehow
+    // return fromFetch('http://localhost:9898/test', {
+    return fromFetch('http://openshift-console-plugin-test-svc.plugin--openshift-console-plugin-test.svc:9898/test', {
       method: 'GET'
     }).pipe(
       concatMap((resp: Response) => resp.ok ? resp.text() : of(resp.statusText)),
