33993041dd7b0a24cddd213ea2244d51f9219594
Andrew Azores <aazores@redhat.com>
2024-07-15 15:16:37 -0400

    add notes

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -4,8 +4,8 @@ import { consoleFetch } from '@openshift-console/dynamic-plugin-sdk';
 
 export class ApiService {
   getTest(): Observable<string> {
-    const pluginName = 'cryostat';
-    const proxyAlias = 'cryostat-proxy';
+    const pluginName = 'cryostat'; // this must match the consolePlugin.name in package.json
+    const proxyAlias = 'cryostat-proxy'; // this must match the .spec.proxy.alias in the ConsolePlugin CR
     return from(
       consoleFetch(`/api/proxy/plugin/${pluginName}/${proxyAlias}/test?testQuery=${+Date.now()}`, {
         method: 'GET',
