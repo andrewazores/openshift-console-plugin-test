a42d7c017aff56148b3988b4395526bd67a57a4a
Andrew Azores <aazores@redhat.com>
2024-08-01 08:12:30 -0400

    multiarch

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@ set -xe
 
 DIR="$(dirname "$(readlink -f "$0")")"
 
-podman build "${DIR}" -t quay.io/andrewazores/openshift-console-plugin-test:latest -f "${DIR}/Dockerfile"
+podman buildx build --platform="${PLATFORMS:-linux/amd64,linux/arm64}" --manifest quay.io/andrewazores/openshift-console-plugin-test:latest -f "${DIR}/Dockerfile" "${DIR}"
