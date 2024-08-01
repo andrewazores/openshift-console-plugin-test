f5f4c349100c1377552ab0ef36a9afe9544e8dc2
Andrew Azores <aazores@redhat.com>
2024-08-01 10:35:41 -0400

    replace previous manifest

--- build.bash
+++ build.bash
@@ -3,10 +3,15 @@
 set -xe
 
 DIR="$(dirname "$(readlink -f "$0")")"
+MANIFEST="${MANIFEST:-quay.io/andrewazores/openshift-console-plugin-test:latest}"
+
+if podman manifest exists "${MANIFEST}"; then
+    podman manifest rm "${MANIFEST}"
+fi
 
 podman buildx build \
     --platform="${PLATFORMS:-linux/amd64,linux/arm64}" \
-    --manifest quay.io/andrewazores/openshift-console-plugin-test:latest \
+    --manifest "${MANIFEST}" \
     --file "${DIR}/Dockerfile" \
     --pull=always \
     --ignorefile "${DIR}/.dockerignore" \
