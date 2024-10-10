c5d6711a1e5604bae7c102dd4d6e07274e7787cf
aptmac <almacdon@redhat.com>
2024-10-10 11:32:59 -0400

    revert quay username change

--- build.bash
+++ build.bash
@@ -3,7 +3,7 @@
 set -xe
 
 DIR="$(dirname "$(readlink -f "$0")")"
-MANIFEST="${MANIFEST:-quay.io/almacdon/openshift-console-plugin-test:latest}"
+MANIFEST="${MANIFEST:-quay.io/andrewazores/openshift-console-plugin-test:latest}"
 
 if podman manifest exists "${MANIFEST}"; then
     podman manifest rm "${MANIFEST}"
