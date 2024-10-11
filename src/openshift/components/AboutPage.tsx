9f935fb7fb2b24fc993c74bfb8bc5969cd4a3e1f
aptmac <almacdon@redhat.com>
2024-10-11 12:48:57 -0400

    working on about page

---
+++ src/openshift/components/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { About } from '../../cryostat-web/src/app/About/About';
+import { Page } from '@patternfly/react-core';
+import React from 'react';
+// import '@app/app.css';
+
+export default function AboutPage() {
+  return (
+    <Page>
+      <About></About>
+    </Page>
+  );
+}
