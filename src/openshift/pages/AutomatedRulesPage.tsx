1c00cfd59a176433d514dfa24c47947539c00dae
aptmac <almacdon@redhat.com>
2024-10-25 12:34:33 -0400

    add basic rules page

--- src/openshift/pages/AutomatedRulesPage.tsx
+++ src/openshift/pages/AutomatedRulesPage.tsx
@@ -1,7 +1,12 @@
-import { Page } from '@patternfly/react-core';
 import React from 'react';
 import '@app/app.css';
+import { CryostatContainer } from '../components/CryostatContainer';
+import Rules from '@app/Rules/Rules';
 
 export default function AutomatedRulesPage() {
-  return <Page>TODO: Automated Rules Page</Page>;
+  return (
+    <CryostatContainer>
+      <Rules />
+    </CryostatContainer>
+  );
 }
